b903fc27f7e281ccf470fa90d933524b75735472
sjcineas <101082903+sjcineas@users.noreply.github.com>
2025-03-27 22:11:32 -0400

    Initial Commit

---
+++ TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Schadrack Cineas on 3/27/25.
+//
+
+import Testing
+@testable import TicTacToe
+
+struct TicTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
